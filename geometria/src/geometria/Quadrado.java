fe78c0a4ba0bcbe51f9fbaf639c94c81b65f914c
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2022-04-26 19:45:21 -0300

    Update Quadrado.java

--- geometria/src/geometria/Quadrado.java
+++ geometria/src/geometria/Quadrado.java
@@ -2,34 +2,34 @@ package geometria;
 
 public class Quadrado {
 	// Atributos
-	private double lado;
-
-	// Métodos
-	public void setLado(double lado) {
-		if (lado > 0) {
-			this.lado = lado;
-		}
-	}
-
-	public double calculaArea() {
-		double area = lado * lado;
-
-		return area; 
-	}
-
-	public void desenhar() {
-		for (int i = 0; i < lado; i++) {
-			System.out.print(". ");
-
-			for (int j = 0; j < lado; j++) {
-				if (i == 0 || i + 1 >= lado) {
-					System.out.print(". ");
-				} else {
-					System.out.print("  ");
-				}
+		private double lado;
+			
+		// MÃ©todos
+		public void setLado(double lado) {
+			if (lado > 0) {
+				this.lado = lado;
 			}
-
-			System.out.println(".");
 		}
-	}
+			
+		public double calculaArea() {
+			double area = lado * lado;
+				
+			return area; 
+		}
+			
+		public void desenhar() {
+			for (int i = 0; i < lado; i++) {
+				System.out.print(". ");
+					
+				for (int j = 0; j < lado; j++) {
+					if (i == 0 || i + 1 >= lado) {
+						System.out.print(". ");
+					} else {
+						System.out.print("  ");
+					}
+				}
+					
+				System.out.println(".");
+			}
+		}
 }
