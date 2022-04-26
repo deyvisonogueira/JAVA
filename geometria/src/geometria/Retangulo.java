f00dea82b46b7295e236157e3517e71a2a4aaf86
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2022-04-26 19:45:59 -0300

    Update Retangulo.java

--- geometria/src/geometria/Retangulo.java
+++ geometria/src/geometria/Retangulo.java
@@ -2,40 +2,41 @@ package geometria;
 
 public class Retangulo {
 	// Atributos
-	
-	private double lado, altura;
-	
-	// Métodos
-	public void setLado(double lado) {
-		if (lado > 0) {
-			this.lado = lado;
-		}
-	}
-	public void setAltura(double altura) {
-		if (altura > 0) {
-			this.altura = altura;
-		}
-	}
-	
-	public double calculaArea() {
-		double area = lado * altura;
+		private double base;
+		private double altura;
 		
-		return area; 
-	}
-	
-	public void desenhar() {
-		for (int i = 0; i < altura; i++) {
-			System.out.print(". ");
-			
-			for (int j = 0; j < lado; j++) {
-				if (i == 0 || i + 1 >= altura) {
-					System.out.print(". ");
-				} else {
-					System.out.print("  ");
-				}
+		// MÃ©todos
+		public void setBase(double base) {
+			if (base > 0) {
+				this.base = base;
 			}
-			
-			System.out.println(".");
 		}
-	}
+		
+		public void setAltura(double altura) {
+			if (altura > 0) {
+				this.altura = altura;
+			}
+		}
+		
+		public double calculaArea() {
+			double area = base * altura;
+				
+			return area; 
+		}
+		
+		public void desenhar() {
+			for (int i = 0; i < base; i++) {
+				System.out.print(". ");
+					
+				for (int j = 0; j < altura; j++) {
+					if (i == 0 || i + 1 >= altura) {
+						System.out.print(". ");
+					} else {
+						System.out.print("  ");
+					}
+				}
+					
+				System.out.println(".");
+			}
+		}
 }
